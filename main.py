db7d92b0d0b5427987ed91933323882c1c09b9c6
Nikita <niki.c2000@yandex.ru>
2023-03-12 17:08:22 +0400

    Начало работы с базой данных. добавление пользователей бота в базу данных

--- main.py
+++ main.py
@@ -2,40 +2,80 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram_calendar import simple_cal_callback, SimpleCalendar, \
     dialog_cal_callback, DialogCalendar
 from aiogram.dispatcher.filters import Text
+import psycopg2
 import const
 import GUI
+from config import host, user, password, db_name
 
 bot = Bot(token=const.TOKEN_BOT, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
+conaction = psycopg2.connect(
+    host=host,
+    user=user,
+    password=password,
+    database=db_name
+)
 try:
     @dp.message_handler(commands=['start'])
     async def start(message: types.Message):
-        await message.answer("<em>Добро пожаловать!\n"
-                             "Данный бот создан для вашего учета расходов.\n"
-                             "Вы можете добавлять свои сведения о "
-                             "расходах и доходах.\n"
-                             "Также вы можете запрашивать "
-                             "статистику и историю операций.</em>",
-                             reply_markup=GUI.main_kb,
-                             parse_mode="HTML")
+        user_id = message.from_user.id
+        chat_id = message.chat.id
 
-        # TODO: добавление пользователя в базу данных
+        try:
+            with conaction.cursor() as cursor:
+                cursor.execute(
+                    f"INSERT INTO Users (user_id, chat_id) VALUES {user_id, chat_id};"
+                )
+                conaction.commit()
+                cursor.execute(
+                    "select * from users"
+                )
+                print("Пользователь добавлен в базу данных: ",
+                      cursor.fetchall())
+
+        except psycopg2.errors.UniqueViolation:
+            await message.answer("<em>Добро пожаловать!\n"
+                                 "Мы рады, что вы к нам вернулись!!!\n"
+                                 "Вы можете добавлять свои сведения о "
+                                 "расходах и доходах.\n"
+                                 "Также вы можете запрашивать "
+                                 "статистику и историю операций.</em>",
+                                 reply_markup=GUI.main_kb,
+                                 parse_mode="HTML")
+        else:
+            await message.answer("<em>Добро пожаловать!\n"
+                                 "Данный бот создан для вашего учета расходов.\n"
+                                 "Вы можете добавлять свои сведения о "
+                                 "расходах и доходах.\n"
+                                 "Также вы можете запрашивать "
+                                 "статистику и историю операций.</em>",
+                                 reply_markup=GUI.main_kb,
+                                 parse_mode="HTML")
+
+
+    @dp.message_handler(commands=['stop-conaction'])
+    async def start(message: types.Message):
+        conaction.close()
+        print("Соединение закрыто")
 
 
     @dp.message_handler(text='История')
     async def History(message: types.Message):
-        await message.answer("Выберите категорию", reply_markup=GUI.history_kb)
+        await message.answer("Выберите категорию",
+                             reply_markup=GUI.history_kb)
 
 
     @dp.message_handler(text='История доходов')
     async def History_Income(message: types.Message):
-        await message.answer("Выберите категорию", reply_markup=GUI.time_kb)
+        await message.answer("Выберите категорию",
+                             reply_markup=GUI.time_kb)
 
 
     @dp.message_handler(text='История расходов')
     async def History_Expenses(message: types.Message):
-        await message.answer('Выберите категорию', reply_markup=GUI.time_kb)
+        await message.answer('Выберите категорию',
+                             reply_markup=GUI.time_kb)
 
 
     @dp.message_handler(text='Добавить доходы')
@@ -97,6 +137,10 @@ try:
     @dp.message_handler(text='Связь с разработчиком')
     async def contact_developer(message: types.Message):
         await message.answer('@Sad_prod')
+
+
+except Exception as ex_:
+    print("Error while working with PostgreSQL:", ex_)
 except:
     print('Exept in main.py')
 
